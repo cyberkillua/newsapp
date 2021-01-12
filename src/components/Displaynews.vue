b54a4fcea0dc069c91921ee0f9e198089867a792
cyberkillua <moshood988@gmail.com>
2021-01-12 12:00:34 +0100

    new commit

--- src/components/Displaynews.vue
+++ src/components/Displaynews.vue
@@ -1,22 +1,23 @@
 <template>
-  <div class="onecard">
+  <a
+    v-bind:href="article.url"
+    target="_blank"
+    rel="noopener noreferrer"
+    title="domain"
+  >
+    <div class="onecard">
       <div class="imgcard">
-    <img v-bind:src="article.image" />
+        <img v-bind:src="article.image" />
       </div>
-    <div class="textcard">
-    <h2>{{ article.title }}</h2>
-    <p>{{ article.description}}</p>
-    <p>
-      <a
-        v-bind:href="article.url"
-        target="_blank"
-        rel="noopener noreferrer"
-        title="domain"
-        >{{ article.source.name }}</a
-      >
-    </p>
+      <div class="textcard">
+        <h2>{{ article.title }}</h2>
+        <p>{{ article.description }}</p>
+        <p id="namecard">
+          {{ article.source.name }}
+        </p>
+      </div>
     </div>
-  </div>
+  </a>
 </template>
 
 <script>
@@ -27,27 +28,31 @@ export default {
 </script>
 
 <style  scoped>
+a {
+  text-decoration: none;
+  color: #fafafa;
+}
 .onecard {
   display: flex;
-   margin-bottom: 50px;
-  font-family: 'Fira Sans', sans-serif;
+  margin-bottom: 50px;
+  font-family: "Fira Sans", sans-serif;
 }
-a {
-  color: #ff9933;
-  text-decoration: none;
-  font-style: italic;
-  padding-top: 10px;
-}
+
 img {
   height: 250px;
   width: 300px;
   margin: 10px;
 }
 .textcard {
-    padding: 0 10px 0 10px;
-    height: 250px;
-    width: 40%;
-    margin: 0 10px 10px 50px;
+  padding: 0 10px 0 10px;
+  height: 250px;
+  width: 40%;
+  margin: 0 10px 10px 50px;
+}
+#namecard {
+  color: #ff9933;
+  font-style: italic;
+  padding-top: 10px;
 }
 @media screen and (max-width: 768px) {
   .onecard {
@@ -60,19 +65,17 @@ img {
     width: 80%;
     margin: auto;
     /* background-color: green; */
-    
-}
-.imgcard {
+  }
+  .imgcard {
     width: 80%;
     margin: auto;
     /* background-color: tomato; */
-}
+  }
   img {
     height: 150px;
     width: 100%;
     padding: 0;
     margin: 10px 0 10px 0;
-
   }
 }
 </style>
