c0c789e6b133214774ff90ff2cce9de58db74ca7
cyberkillua <moshood988@gmail.com>
2021-01-12 12:01:06 +0100

    new commit

--- src/App.vue
+++ src/App.vue
@@ -56,11 +56,11 @@ body {
 .navcard {
   background-color: #262626;
   width: 100%;
-  height: 100px;
+  height: 80px;
   margin-bottom: 70px;
   color: #ff9933;
   font-family: "Montserrat", sans-serif;
-  font-size: xx-large;
+  font-size: larger;
   justify-content: center;
   align-items: center;
   display: flex;
